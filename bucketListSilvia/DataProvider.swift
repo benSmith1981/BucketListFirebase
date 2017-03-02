b805111c0371cb5338bdc6598c2958ba5db4eb35
Maya Lekova <apokalyptra@gmail.com>
2017-03-02 12:24:58 +0100

    Bucket wishes are now stored as a dictionary instead of array
    * Add temporary method for converting the existing data
    * Use the “BucketWishesDict” key instead of “BucketWishes”
    … as explained in the “Best Practices: Arrays in Firebase” blogpost

--- bucketListSilvia/DataProvider.swift
+++ bucketListSilvia/DataProvider.swift
@@ -22,17 +22,47 @@ class DataProvider {
         
         ref.observeSingleEvent(of: .value, with: { (snapshot) in
             if let dataDictionarys = snapshot.value as? NSDictionary {
-               let bucketArray = dataDictionarys["BucketWishes"]
-               let bucketlistItemsArray = BucketWishes.modelsFromDictionaryArray(array: bucketArray as! NSArray)
+                // 3. I changed the key that we use here (the "path to child" string)
+                let bucketDict = dataDictionarys["BucketWishesDict"] as! NSDictionary
+                let bucketArray = bucketDict.allValues
+                let bucketlistItemsArray = BucketWishes.modelsFromDictionaryArray(array: bucketArray as NSArray)
                 
                 let bucketListData = ["BucketWishes": bucketlistItemsArray]
                 NotificationCenter.default.post(name: Notification.Name(rawValue: "BucketWishesnotify"),
                                                 object: self ,
                                                 userInfo: bucketListData)
 
+                // 1. I called the "convertData" method only once to convert the data
+                // from an array to a dictionary and put it back in Firebase
+                // on a separate key ("BucketWishesDict" instead of "BucketWishes").
+                // Compare the two data representations in the Firebase console
+                // TODO: Jeroen go to Firebase console and delete the "BucketWishes" entry (NOT the "BucketWishesDict" one!!!)
+                
+                // self.convertData(bucketlistItemsArray)
             } else {
                 print("Error while retrieving data from Firebase")
             }
         })
+    }
+    
+    func convertData(_ wishesArray: [BucketWishes]) {
+        var transformed = Dictionary<String, Any>()
+        
+        for wishItem in wishesArray {
+            if let wish = wishItem.wish {
+                
+                // 2. Transform the wishItem (of type BucketWishes) into a Dictionary
+                // so that Firebase can store it; provide default values for optionals
+                var wishObj = Dictionary<String, Any>()
+                wishObj["Wish"] = wishItem.wish ?? "Something"
+                wishObj["Place"] = wishItem.place ?? "Somewhere"
+                wishObj["When"] = wishItem.when ?? 2020
+                
+                transformed[wish] = wishObj
+            }
+        }
+        
+        // This is how we actually store the new dictionary of wishes
+        ref.child("BucketWishesDict").setValue(transformed)
     }
 }
